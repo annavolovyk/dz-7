74bfb5a8587cf3ff23f949a4f7a74b49cded6077
Anna <77854070+annavolovyk@users.noreply.github.com>
2023-05-02 00:23:35 +0300

    Update CardRank.java

--- src/CardRank.java
+++ src/CardRank.java
@@ -1,27 +1,27 @@
 package src;
 
 public enum CardRank {
-    ACE ("1"),
-    TWO ("2"),
-    THREE ("3"),
-    FOUR ("4"),
-    FIVE ("5"),
-    SIX ("6"),
-    SEVEN ("7"),
-    EIGHT ("8"),
-    NINE ("9"),
-    TEN ("10"),
-    JACK ("11"),
-    QUEEN ("12"),
-    KING ("13");
+    ACE (1),
+    TWO (2),
+    THREE (3),
+    FOUR (4),
+    FIVE (5),
+    SIX (6),
+    SEVEN (7),
+    EIGHT (8),
+    NINE (9),
+    TEN (10),
+    JACK (11),
+    QUEEN (12),
+    KING (13);
 
-    private final String numbers;
+    private final int numbers;
 
-    CardRank(String numbers) {
+    CardRank(int numbers) {
         this.numbers = numbers;
     }
 
-    public String getNumbers() {
+    public int getNumbers() {
 
         return numbers;
     }
